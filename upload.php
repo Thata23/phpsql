568623d271436da8deda3b03055c0c06ff633630
Thaissa <you@example.com>
2020-11-30 11:01:03 -0300

    Modificação no upload

--- upload.php
+++ upload.php
@@ -4,10 +4,10 @@ include_once('conexao.php');
 
 $nome_final= $_POST["nomeI"];
 
-if (substr($_FILES['arquivo']['nomeI'], -3) == "png" ||substr($_FILES['arquivo']['nomeI'], -3) == "jpg"){
+if (substr($_FILES['arquivo']['name'], -3) == "png" ||substr($_FILES['arquivo']['name'], -3) == "jpg"){
     $dir = './arquivos/';
     $tmpName = $_FILES['arquivo']['tmp_name'];
-    $name = $_FILES['arquivo']['nomeI'];
+    $name = $_FILES['arquivo']['name'];
     // move_uploaded_file
     if (move_uploaded_file($tmpName, $dir . $nome_final)) {
          $sqlstring = "insert into imagens (id, arquivo, nomeI) values (null, '$nome_final')";
