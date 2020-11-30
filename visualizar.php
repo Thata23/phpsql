a2693315a78c335eb4124eebcf9048da7078fc40
Thaissa <you@example.com>
2020-11-30 10:28:56 -0300

    Visualização

---
+++ visualizar.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once('conexao.php');
+$id = $_GET['id'];
+
+// recuperando o nome do arquivo 
+$sqlselect = "select * from imagens where id=$id";
+$resultado = mysqli_query($conexao, $sqlselect);
+$dados = mysqli_fetch_array($resultado);
+$nome = $dados['arquivo'];
+
+// excluindo o registro
+$sqlvisualizar = "insert into imagens (nomeI) values (null, '$nome_final')";
+mysqli_query($conexao, $sqlvisualizar);
+
+unlink('arquivos/' . $nome);
+header('Location: index.php');
+?>
