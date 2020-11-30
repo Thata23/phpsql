a967cd9655623265d3edfd3eb698261e91858093
Thaissa <you@example.com>
2020-11-30 10:23:07 -0300

    Modificação das variaveis

--- upload.php
+++ upload.php
@@ -4,13 +4,13 @@ include_once('conexao.php');
 
 $nome_final= $_POST["nomeI"];
 
-if (substr($_FILES['arquivo']['name'], -3) == "png" ||substr($_FILES['arquivo']['name'], -3) == "jpg"){
+if (substr($_FILES['arquivo']['nomeI'], -3) == "png" ||substr($_FILES['arquivo']['nomeI'], -3) == "jpg"){
     $dir = './arquivos/';
     $tmpName = $_FILES['arquivo']['tmp_name'];
     $name = $_FILES['arquivo']['name'];
     // move_uploaded_file
     if (move_uploaded_file($tmpName, $dir . $nome_final)) {
-         $sqlstring = "insert into imagens (id, arquivo) values (null, '$nome_final')";
+         $sqlstring = "insert into imagens (id, arquivo, nomeI) values (null, '$nome_final')";
         mysqli_query($conexao, $sqlstring);
         header('Location: index.php');
     } else {
