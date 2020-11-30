eb896e797a1c427b5b9d101fb7dbb38bd829a493
Thaissa <you@example.com>
2020-11-30 15:20:59 -0300

    Index modificação

--- visualizar.php
+++ visualizar.php
@@ -3,16 +3,5 @@
 include_once('conexao.php');
 $id = $_GET['id'];
 
-// recuperando o nome do arquivo 
-$sqlselect = "select * from imagens where id=$id";
-$resultado = mysqli_query($conexao, $sqlselect);
-$dados = mysqli_fetch_array($resultado);
-$nome = $dados['arquivo'];
-
-// excluindo o registro
-$sqlvisualizar = "select from imagens where nomeI='$id'";
-mysqli_query($conexao, $sqlvisualizar);
-
-unlink('arquivos/' . $nome);
-header('Location: index.php');
+echo "<img src='arquivos/" . $id['arquivo'] . "' width='100px' heigth='100px'>";
 ?>
